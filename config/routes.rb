71cd3c411e58146b0c5e957bb454df88dfa086c0
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-25 19:33:51 +0530

    Add dashboard for meter_readers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'dashboard#index'
+  get '/dashboard', to: 'dashboard#index'
+  get 'dashboard/input', to: 'dashboard#input'
   devise_for :meter_readers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
