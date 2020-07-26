e14d49fa8f63a5cdd4381a3dfd2b82db13eeb806
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-07-26 12:25:26 +0530

    Add route and form
    ... for meter unit submission functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'dashboard#index'
   get '/dashboard', to: 'dashboard#index'
-  get 'dashboard/input', to: 'dashboard#input'
   post 'dashboard/search', to: 'dashboard#search'
+  get 'dashboard/input', to: 'dashboard#input'
+  post 'dashboard/submit', to: 'dashboard#submit'
   devise_for :meter_readers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
